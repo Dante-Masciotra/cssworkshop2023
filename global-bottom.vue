d601dd7885abf93ff9e159deb82bdc57e225f1f8
Borhan Saflo <60056206+BorhanSaflo@users.noreply.github.com>
2023-09-06 20:37:02 -0400

    feat: Slide Number

---
+++ global-bottom.vue
@@ -0,0 +1,9 @@
+<template>
+    <footer
+      v-if="$slidev.nav.currentLayout !== 'cover'"
+      class="absolute bottom-0 right-0 p-2 text-blue-600 font-bold"
+    >
+      {{ $slidev.nav.currentPage }}/{{ $slidev.nav.total }}
+    </footer>
+  </template>
+  
